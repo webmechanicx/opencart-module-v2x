f047582a40fe8d731a6163dec4c15e92f40c33a5
Farhadur Rahim <varendrasoft@gmail.com>
2016-05-12 19:28:48 +0600

    add language file

---
+++ admin/language/en-gb/module/helloworld.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title']       = 'Hello World';
+ 
+// Text
+$_['text_module']         = 'Modules';
+$_['text_edit']           = 'Edit Hello World Module';
+$_['text_success']        = 'Success: You have modified module Hello World!';
+ 
+// Entry
+$_['entry_code']          = 'Hello World Code:';
+$_['entry_status']        = 'Status:';
+ 
+// Error
+$_['error_permission']    = 'Warning: You do not have permission to modify module Hello World!';
+$_['error_code']          = 'Code Required';
