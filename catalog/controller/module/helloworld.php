a7f827336992f2c8649fe6abd0422533a821e752
Farhadur Rahim <varendrasoft@gmail.com>
2016-05-12 20:36:24 +0600

    Update helloworld.php

--- catalog/controller/module/helloworld.php
+++ catalog/controller/module/helloworld.php
@@ -6,11 +6,7 @@ class ControllerModuleHelloworld extends Controller {
         $data['heading_title'] = $this->language->get('heading_title'); // set the heading_title of the module
          
         $data['helloworld_value'] = html_entity_decode($this->config->get('helloworld_text_field')); // gets the saved value of helloworld text field and parses it to a variable to use this inside our module view
-         
-        if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/module/helloworld.tpl')) {
-            return $this->load->view($this->config->get('config_template') . '/template/module/helloworld.tpl', $data);
-        } else {
-            return $this->load->view('default/template/module/helloworld.tpl', $data);
-        }
+        
+            return $this->load->view('module/helloworld', $data);
     }
 }
